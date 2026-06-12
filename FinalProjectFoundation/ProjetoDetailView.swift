c12879eccbd4eb472cde5ad14c8e1bf4c877df0c
ArturH16 <holandaartur163@gmail.com>
2026-06-12 13:59:55 -0300

    criacao da tela de detalhes de projeto

---
+++ FinalProjectFoundation/ProjetoDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  ProjetoDetailsView.swift
+//  FinalProjectFoundation
+//
+//  Created by Found on 12/06/26.
+//
+
+import SwiftUI
+
+struct ProjetoDetailView: View {
+    let projeto: Projeto
+    
+    var body: some View {
+        
+    }
+}
